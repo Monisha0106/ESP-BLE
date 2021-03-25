66d8c3fd2b727141dec8e99586b47ee86c1c3cc7
Foysal Khandakar Joy <khjoyfoysal@gmail.com>
2021-03-25 12:14:11 +0600

    tried to connect to wifi

--- src/main.cpp
+++ src/main.cpp
@@ -20,21 +20,21 @@
    And txValue is the data to be sent, in this example just a byte incremented every second. 
 */
 #include<Arduino.h>
-#include <BLEDevice.h>
-#include <BLEServer.h>
-#include <BLEUtils.h>
-#include <BLE2902.h>
+// #include <BLEDevice.h>
+// #include <BLEServer.h>
+// #include <BLEUtils.h>
+// #include <BLE2902.h>
 #include <EEPROM.h>
 #include <WiFi.h>
 #include <PubSubClient.h>
 
 const char *ssid =  "dma-gulshan5.0";   // name of your WiFi network
 const char *password =  "dmabd987"; // password of the WiFi network
-const char* mqtt_server = "broker.hivemq.com";
+const char *mqtt_server = "broker.hivemq.com";
 // IPAddress broker(192,168,1,-); // IP address of your MQTT broker eg. 192.168.1.50
-WiFiClient espclient;
+WiFiClient wclient;
 
-PubSubClient client(espclient); // Setup MQTT client
+PubSubClient client(wclient); // Setup MQTT client
 bool state=0;
 unsigned long lastMsg = 0;
 #define MSG_BUFFER_SIZE	(50)
@@ -42,7 +42,7 @@ char msg[MSG_BUFFER_SIZE];
 int value = 0;
 
 int address=0;
-BLECharacteristic *pCharacteristic;
+// BLECharacteristic *pCharacteristic;
 bool deviceConnected = false;
 float txValue = 0;
 const int readPin = 32; // Use GPIO number. See ESP32 board pinouts
@@ -56,7 +56,7 @@ const int Gate = 2; // Could be different depending on the dev board. I used the
 #define SERVICE_UUID           "6E400001-B5A3-F393-E0A9-E50E24DCCA9E" // UART service UUID
 #define CHARACTERISTIC_UUID_RX "6E400002-B5A3-F393-E0A9-E50E24DCCA9E"
 #define CHARACTERISTIC_UUID_TX "6E400003-B5A3-F393-E0A9-E50E24DCCA9E"
-
+/*
 class MyServerCallbacks: public BLEServerCallbacks {
     void onConnect(BLEServer* pServer) {
       deviceConnected = true;
@@ -71,6 +71,7 @@ struct ID{
 };
 struct ID employee;
 int count=0;
+
 class MyCallbacks: public BLECharacteristicCallbacks {
     void onWrite(BLECharacteristic *pCharacteristic) {
       std::string rxValue = pCharacteristic->getValue();
@@ -103,7 +104,7 @@ class MyCallbacks: public BLECharacteristicCallbacks {
       }
     }
 };
-
+*/
 void setup_wifi() {
 
   delay(10);
@@ -175,13 +176,13 @@ void callback(char* topic, byte* payload, unsigned int length) {
 
 
 void setup() {
-  Serial.begin(115200);
+  Serial.begin(9600);
   setup_wifi();
   client.setServer(mqtt_server, 1883);
   client.setCallback(callback);
 
   pinMode(Gate, OUTPUT);
-
+  /*
   // Create the BLE Device
   BLEDevice::init("Uttsha's BLE"); // Give it a name
 
@@ -213,7 +214,7 @@ void setup() {
   // Start advertising
   pServer->getAdvertising()->start();
   Serial.println("Waiting a client connection to notify...");
-
+*/
 
 }
 
@@ -227,10 +228,12 @@ void loop() {
   if (now - lastMsg > 2000) {
     lastMsg = now;
     ++value;
-    snprintf (msg, MSG_BUFFER_SIZE, "hello world #%ld", value);
+    // snprintf (msg, MSG_BUFFER_SIZE, "hello world #%lld", value);
+    // snprintf (msg, MSG_BUFFER_SIZE, "hello world #%lld");
+    Serial.println(msg);
     Serial.print("Publish message: ");
     Serial.println(msg);
-    client.publish("bl21", msg);
+    client.publish("ble1", msg);
   }
   /*
   BLEScan *scan = BLEDevice::getScan();
